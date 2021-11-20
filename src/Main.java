167e09999c1bf5b515d8d341bd6ac635c9b9d2e9
Douglas9197 <douglas_wilham@yahoo.com.br>
2021-11-20 18:55:36 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        double a,b;
+        double total;
+
+        System.out.println("calcladora");
+
+        System.out.println("digite 1 para (*), digite 2 para (/), digie 3 para (+), digite 4 (-)");
+        num = scan.nextInt();
+
+        if (num == 1 ){
+
+            System.out.println("informe o valor de A: ");
+            a = scan.nextDouble();
+
+            System.out.println("informe o valor de B: ");
+            b = scan.nextDouble();
+
+            total = a * b;
+            System.out.println("o total da multiplicaçao e: "+total);
+
+
+
+
+
+        }
+
+
+
+    }
+}
